26f661b798781deb9bcdaeb8fb42ae0c8f626db3
Karthikeyan Chinnakonda <karthikeyan@hasura.io>
2020-02-16 12:26:56 +0530

    change the structure of the scheduled-trigger tests

--- server/tests-py/test_scheduled_triggers.py
+++ server/tests-py/test_scheduled_triggers.py
@@ -21,80 +21,116 @@ def get_events_of_scheduled_trigger(hge_ctx,trigger_name):
     }
     return hge_ctx.v1q(q)
 
-def check_schedule_of_generated_events(hge_ctx,trigger_name,cron,time):
-    future_schedule_timestamps = []
-    iter = croniter(cron,time)
-    sql = '''
-    select scheduled_time from hdb_catalog.hdb_scheduled_events where name = '{}' order by scheduled_time asc limit 5;
-    '''
-    q = {
-        "type":"run_sql",
-        "args":{
-            "sql":sql.format(trigger_name)
-        }
-    }
-    for i in range(5):
-        future_schedule_timestamps.append(iter.next(datetime))
-    st,resp = hge_ctx.v1q(q)
-    assert st == 200
-    ts_resp = resp['result'][1:]
-    scheduled_events_ts = []
-    for ts in ts_resp:
-        datetime_ts = datetime.strptime(ts[0],"%Y-%m-%d %H:%M:%S")
-        scheduled_events_ts.append(datetime_ts)
-    assert future_schedule_timestamps == scheduled_events_ts
-
-
-def check_if_scheduled_event_exists(hge_ctx,trigger_name,retries=1,interval=4.0):
-    while (retries > 0):
-        st,resp = get_events_of_scheduled_trigger(hge_ctx,trigger_name)
-        if int(resp['result'][1][0]) == 1:             #parses the count from the commented_map
-            return
-        time.sleep(interval)
-        retries = retries -1
-    assert False       # not found any scheduled events, so fail the test
-
 class TestSubscriptionTrigger(object):
 
-    def test_cron_scheduled_trigger(self,hge_ctx,evts_webhook):
+    cron_trigger_name = ""
+    adhoc_trigger_name = ""
+    cron_schedule = "5 * * * *"
+    init_time = datetime.now()
+
+    def test_create_schedule_triggers(self,hge_ctx):
         current_time_str = stringify_datetime(datetime.now())
-        trigger_name = "a scheduled trigger - " + current_time_str
-        cron_schedule = "5 * * * *"
-        q = {
+        TestSubscriptionTrigger.cron_trigger_name = "a scheduled trigger - " + current_time_str
+        TestSubscriptionTrigger.adhoc_trigger_name = "adhoc trigger - " + current_time_str
+        TestSubscriptionTrigger.cron_schedule = "5 * * * *"
+        cron_st_api_query = {
             "type":"create_scheduled_trigger",
             "args":{
-                "name":trigger_name,
+                "name":self.cron_trigger_name,
                 "webhook":"http://127.0.0.1/5592",
                 "schedule":{
                     "type":"cron",
-                    "value":cron_schedule
+                    "value":self.cron_schedule
                 }
             },
             "payload":"{}"
         }
-        url = '/v1/query'
-        st_code,st_resp,_ = hge_ctx.anyq(url,q,{})
-        assert st_code == 200
-        time.sleep(60.0)
-        check_schedule_of_generated_events(hge_ctx,trigger_name,cron_schedule,datetime.utcnow())
-
-    def test_one_off_scheduled_trigger(self,hge_ctx,evts_webhook):
-        time = datetime.now()
-        current_time_str = stringify_datetime(time)
-        trigger_name = "adhoc_trigger-" + current_time_str
-        create_trigger_query = {
+        adhoc_st_api_query = {
             "type":"create_scheduled_trigger",
             "args":{
-                "name":trigger_name,
+                "name":self.adhoc_trigger_name,
                 "webhook":"http://127.0.0.1/5592",
                 "schedule":{
                     "type":"adhoc",
                     "value":current_time_str
                 },
             },
-                "payload":"{\"foo\":\"baz\"}"
+            "payload":"{\"foo\":\"baz\"}"
         }
         url = '/v1/query'
-        st_code,resp,_ = hge_ctx.anyq(url,create_trigger_query,{})
-        assert st_code == 200,resp
-        check_if_scheduled_event_exists(hge_ctx,trigger_name,10,1.0) #max wait time for 10s
+        cron_st_code,cron_st_resp,_ = hge_ctx.anyq(url,cron_st_api_query,{})
+        TestSubscriptionTrigger.init_time = datetime.utcnow()
+        adhoc_st_code,adhoc_st_resp,_ = hge_ctx.anyq(url,adhoc_st_api_query,{})
+        assert cron_st_code == adhoc_st_code == 200
+        assert cron_st_resp['message'] ==  adhoc_st_resp['message'] == 'success'
+        time.sleep(60.0)
+
+    def test_check_generated_scheduled_events(self,hge_ctx):
+        future_schedule_timestamps = []
+        iter = croniter(self.cron_schedule,self.init_time)
+        for i in range(5):
+            future_schedule_timestamps.append(iter.next(datetime))
+        sql = '''
+    select scheduled_time from hdb_catalog.hdb_scheduled_events where name = '{}' order by scheduled_time asc limit 5;
+    '''
+        q = {
+            "type":"run_sql",
+            "args":{
+                "sql":sql.format(self.cron_trigger_name)
+            }
+        }
+        st,resp = hge_ctx.v1q(q)
+        assert st == 200
+        print("resp",resp)
+        ts_resp = resp['result'][1:]
+        scheduled_events_ts = []
+        for ts in ts_resp:
+            datetime_ts = datetime.strptime(ts[0],"%Y-%m-%d %H:%M:%S")
+            scheduled_events_ts.append(datetime_ts)
+        assert future_schedule_timestamps == scheduled_events_ts
+        adhoc_event_st,adhoc_event_resp = get_events_of_scheduled_trigger(hge_ctx,self.adhoc_trigger_name)
+        assert int(adhoc_event_resp['result'][1][0]) == 1
+
+
+    # def test_cron_scheduled_trigger(self,hge_ctx,evts_webhook):
+    #     current_time_str = stringify_datetime(datetime.now())
+    #     trigger_name = "a scheduled trigger - " + current_time_str
+    #     cron_schedule = "5 * * * *"
+    #     q = {
+    #         "type":"create_scheduled_trigger",
+    #         "args":{
+    #             "name":trigger_name,
+    #             "webhook":"http://127.0.0.1/5592",
+    #             "schedule":{
+    #                 "type":"cron",
+    #                 "value":cron_schedule
+    #             }
+    #         },
+    #         "payload":"{}"
+    #     }
+    #     url = '/v1/query'
+    #     st_code,st_resp,_ = hge_ctx.anyq(url,q,{})
+    #     assert st_code == 200
+    #     time.sleep(60.0)
+    #     check_schedule_of_generated_events(hge_ctx,trigger_name,cron_schedule,datetime.utcnow())
+
+    # def test_one_off_scheduled_trigger(self,hge_ctx,evts_webhook):
+    #     time = datetime.now()
+    #     current_time_str = stringify_datetime(time)
+    #     trigger_name = "adhoc_trigger-" + current_time_str
+    #     create_trigger_query = {
+    #         "type":"create_scheduled_trigger",
+    #         "args":{
+    #             "name":trigger_name,
+    #             "webhook":"http://127.0.0.1/5592",
+    #             "schedule":{
+    #                 "type":"adhoc",
+    #                 "value":current_time_str
+    #             },
+    #         },
+    #             "payload":"{\"foo\":\"baz\"}"
+    #     }
+    #     url = '/v1/query'
+    #     st_code,resp,_ = hge_ctx.anyq(url,create_trigger_query,{})
+    #     assert st_code == 200,resp
+    #     check_if_scheduled_event_exists(hge_ctx,trigger_name,10,1.0) #max wait time for 10s
